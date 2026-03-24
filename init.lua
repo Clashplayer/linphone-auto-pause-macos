3459f1539c54c7c2174ee5781d71dd64971df1e8
Clashplayer <69047238+Clashplayer@users.noreply.github.com>
2026-03-24 15:49:11 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,29 +5,32 @@ function mediaPause()
     hs.eventtap.event.newSystemKeyEvent("PLAY", false):post()
 end
 
-function checkLinphoneWindow()
+function isCallActive()
     local app = hs.application.get("Linphone")
-    
-    if app then
-        local win = app:focusedWindow()
-        
-        if win and win:title() == "Appels" then
-            if not paused then
-                mediaPause()
-                paused = true
-            end
-        else
-            if paused then
-                mediaPause()
-                paused = false
-            end
+    if not app then return false end
+
+    for _, win in ipairs(app:allWindows()) do
+        local title = win:title() or ""
+
+        if title == "Appels" then
+            return true
         end
-    else
-        if paused then
-            mediaPause()
-            paused = false
-        end
+    end
+
+    return false
+end
+
+function checkCallState()
+    local callActive = isCallActive()
+
+    if callActive and not paused then
+        mediaPause()
+        paused = true
+
+    elseif not callActive and paused then
+        mediaPause()
+        paused = false
     end
 end
 
-timer = hs.timer.doEvery(0.5, checkLinphoneWindow)
+timer = hs.timer.doEvery(0.3, checkCallState)
